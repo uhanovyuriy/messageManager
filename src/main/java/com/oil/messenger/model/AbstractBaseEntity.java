6a46566ea7309804ea7e6c372e1ba7d07b8b900c
uhanovyuriy <yorikuh@gmail.com>
2020-01-05 16:45:23 +0200

    add AbstractBaseEntity

---
+++ src/main/java/com/oil/messenger/model/AbstractBaseEntity.java
@@ -0,0 +1,41 @@
+package com.oil.messenger.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+@Getter
+@Setter
+public abstract class AbstractBaseEntity {
+
+    public static final int START_SEQ = 100000;
+
+    @Id
+    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
+    protected Integer id;
+
+    protected AbstractBaseEntity() {
+
+    }
+
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return Objects.equals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+}
