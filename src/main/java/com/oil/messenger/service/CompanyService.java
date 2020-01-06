dc2bf022bd3109337ad6898dc1d590b7f5a84e42
uhanovyuriy <yorikuh@gmail.com>
2020-01-06 19:41:45 +0200

    add interface CompanyService

---
+++ src/main/java/com/oil/messenger/service/CompanyService.java
@@ -0,0 +1,20 @@
+package com.oil.messenger.service;
+
+import com.oil.messenger.model.Company;
+
+import java.util.List;
+
+public interface CompanyService {
+
+    Company create(Company company);
+
+    void update(Company company);
+
+    void delete(int id);
+
+    Company findByEmail(String email);
+
+    Company findById(int id);
+
+    List<Company> getAll();
+}
